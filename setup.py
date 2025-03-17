ec04fb2b864ea2b300a80f01fdf1ec6c8c9e54d4
hyborn <hyborninc@gmail.com>
2025-03-17 16:46:58 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="us_visa",
+    version = "0.0.0",
+    author ='hyborn',
+    author_email ="acharafra@gmail.com",
+    packages = find_packages()
+)
