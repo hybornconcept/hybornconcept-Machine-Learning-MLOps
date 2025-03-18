9dc76252420c5f8bbdc969bb684fe3a502cebfb0
hyborn <hyborninc@gmail.com>
2025-03-18 02:27:31 +0100

    mongodb database upload

--- setup.py
+++ setup.py
@@ -7,3 +7,9 @@ setup(
     author_email ="acharafra@gmail.com",
     packages = find_packages()
 )
+
+# hyborn
+# uV2rASfN2K@4VDt
+# Legacy@5187
+
+# mongodb+srv://hyborn:<db_password>@cluster0.fqvxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
